f81204f730d5cef7df4453bdd7f34f7c19c55bfb
ValerioScharizza <valerio.schiarizza@live.it>
2023-02-15 15:58:59 +0100

    show single pizzas

--- src/main/java/com/lessons/java/controller/IndexController.java
+++ src/main/java/com/lessons/java/controller/IndexController.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.lessons.java.model.Pizza;
 import com.lessons.java.repository.PizzaRepository;
@@ -22,5 +24,38 @@ public class IndexController {
 		List<Pizza> pizzaList = pizzaRepository.findAll();
 		model.addAttribute("pizzas", pizzaList);
 		return "index";
+	}
+
+	@GetMapping("/pizzas")
+	public String getPizzasByTitle(@RequestParam(required = false) String title, Model model) {
+		List<Pizza> pizzas;
+		if (title != null && !title.isEmpty()) {
+			pizzas = pizzaRepository.findByNameContainingIgnoreCase(title);
+		} else {
+			pizzas = pizzaRepository.findAll();
+		}
+		model.addAttribute("pizzas", pizzas);
+		return "pizzaIndex";
+	}
+
+	@GetMapping("/pizzas/{id}")
+	public String showPizzaDetails(@PathVariable("id") Integer id, Model model) {
+		Pizza pizza = getPizzaById(id);
+		if (pizza != null) {
+			model.addAttribute("pizza", pizza);
+			return "pizzaDetails";
+		} else {
+			return "redirect:/";
+		}
+	}
+
+	private Pizza getPizzaById(int id) {
+		List<Pizza> pizzas = pizzaRepository.findAll();
+		for (Pizza pizza : pizzas) {
+			if (pizza.getId() == id) {
+				return pizza;
+			}
+		}
+		return null;
 	}
 }
--- src/main/java/com/lessons/java/repository/PizzaRepository.java
+++ src/main/java/com/lessons/java/repository/PizzaRepository.java
@@ -1,9 +1,13 @@
 package com.lessons.java.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.lessons.java.model.Pizza;
 
 public interface PizzaRepository extends JpaRepository<Pizza, Integer> {
+
+	public List<Pizza> findByNameContainingIgnoreCase(String name);
 
 }
